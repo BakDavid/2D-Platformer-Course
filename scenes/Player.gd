648dde5c29c27dd64eaadcfa6c93410ea4b62d04
Bak David <bak.david96@gmail.com>
2022-10-12 00:23:54 +0300

    Refined player movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,24 +1,36 @@
 extends KinematicBody2D
 
-var gravity = 300
+var gravity = 1000
 var velocity = Vector2.ZERO
-var maxHorizontalSpeed = 100
-var jumpSpeed = 200
+var maxHorizontalSpeed = 140
+var horizontalAcceleration = 2000
+var jumpSpeed = 360
+var jumpTerminationMultiplier = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	#player movement
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left")
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
-	velocity.x = moveVector.x * maxHorizontalSpeed
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	if(moveVector.x == 0):
+		velocity.x = lerp(0,velocity.x,pow(2,-50 * delta))
 	
+	#player can only reach a maximum speed
+	velocity.x = clamp(velocity.x,-maxHorizontalSpeed,maxHorizontalSpeed)
+	
+	#player jump
 	if(moveVector.y < 0 && is_on_floor()):
 		velocity.y = moveVector.y * jumpSpeed
 		
-	velocity.y += gravity * delta
+	if(velocity.y < 0 && !Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
+		
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
