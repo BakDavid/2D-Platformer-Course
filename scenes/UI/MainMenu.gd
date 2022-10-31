bcb18afa2f978d59b257c99106a6a2ef1c819b74
Bak David <bak.david96@gmail.com>
2022-10-31 23:41:54 +0200

    Added options menu with fullscreen option

--- scenes/UI/MainMenu.gd
+++ scenes/UI/MainMenu.gd
@@ -13,7 +13,7 @@ func on_play_pressed():
 	$"/root/LevelManager".change_level(0)
 	
 func on_options_pressed():
-	pass
+	$"/root/ScreenTransitionManager".transition_to_scene("res://scenes/UI/OptionsMenuStandalone.tscn")
 	
 func on_quit_pressed():
 	get_tree().quit()
