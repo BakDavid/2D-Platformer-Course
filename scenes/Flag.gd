458ccdf8b9bb7ab5f553d91a33a0aaeba9958cac
Bak David <bak.david96@gmail.com>
2022-12-15 21:45:54 +0200

    Fixed double complete, sound on mute for flag, added mute, removed lvl4e

--- scenes/Flag.gd
+++ scenes/Flag.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 signal player_won
 
+var flag_reached = false
+
 func _ready():
 	$Area2D.connect("area_entered", self, "on_area_entered")
 	
 func on_area_entered(_area2d):
-	emit_signal("player_won")
-	$Particles2D.emitting = true
-	$AudioStreamPlayer.play()
-	$RandomAudioStreamPlayer.play()
+	if(!flag_reached):
+		flag_reached = true
+		emit_signal("player_won")
+		$Particles2D.emitting = true
+		$AudioStreamPlayer.play()
+		$RandomAudioStreamPlayer.play()
--- scenes/Util/RangeControl.gd
+++ scenes/Util/RangeControl.gd
@@ -7,6 +7,7 @@ var currentPercentage = 1.0
 func _ready():
 	$SubtractButton.connect("pressed", self, "on_button_pressed", [-.1])
 	$AddButton.connect("pressed", self, "on_button_pressed", [.1])
+	$MuteButton.connect("pressed", self, "on_mute_button_pressed")
 	
 func set_current_percentage(percent):
 	currentPercentage = clamp(percent, 0, 1)
@@ -17,3 +18,8 @@ func set_current_percentage(percent):
 	
 func on_button_pressed(change):
 	set_current_percentage(currentPercentage + change)
+
+func on_mute_button_pressed():
+	currentPercentage = 0.0
+	$Label.text = str(currentPercentage)
+	emit_signal("percentage_changed", currentPercentage)
